84bba522d381eab5f616c016142c1a6827ff97b5
koloja <dave.folling@gmail.com>
2024-12-08 05:57:15 +0000

    push

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -3,11 +3,20 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import inquirer from 'inquirer';
 
+const componentsLink = 'https://raw.githubusercontent.com/koloja/ui/refs/heads/main/components/[COMPONENT]'
+const components: {[key: string]: string} = {
+    container: 'Container.tsx'
+};
+
 const add = async (component: string | undefined) => {
     if (!component) return logger.error('Please define a component');
+    else if (!components[component]) return logger.error('Invalid component');
     
-
-    logger.info(component);
+    try {
+        const response = await fetch(componentsLink.replace('[COMPONENT]', components[component]));
+        const data = await response.text();
+        console.log(data);
+    } catch (error) {logger.error(`Something went wrong: ${(error as Error).message}`)};
 };
 
 export default add;
