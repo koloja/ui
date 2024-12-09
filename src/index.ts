247f60327521bdeab3ff34b083bd15fd24179795
koloja <dave.folling@gmail.com>
2024-12-09 04:52:01 +0000

    small fixes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ import add from './commands/add';
 const program = new Command();
 
 program.version(pkg.version).description(pkg.description);
-program.command('init').action(() => init());
-program.command('add [component]').description('Add a new component').action(async (component: string) => add(component));
+program.command('init').description('initialize your project (goto https://koloja.dev/ for more info)').action(() => init());
+program.command('add [component]').description('add a new component').action(async (component: string) => add(component));
 program.parse(process.argv);
