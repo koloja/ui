e3528eeae67071498cd159415c096e88f03e9635
koloja <dave.folling@gmail.com>
2024-12-17 06:06:48 +0000

    Update eslint & markdowns

---
+++ packages/eslint/base.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import onlyWarn from 'eslint-plugin-only-warn';
+
+// Global / base eslint config
+/** @type {import('eslint').Linter.Config} */
+export const config = [
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {turbo: turboPlugin},
+        rules: {'turbo/no-undeclared-env-vars': 'warn'}
+    },
+    {plugins: {onlyWarn}},
+    {ignores: ['dist/**']}
+];
